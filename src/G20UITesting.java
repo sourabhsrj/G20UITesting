07c3ee203d0812334df8df09cd24d2a60fa3eff6
SourabhSrj <sourabhj053@gmail.com>
2022-11-04 18:41:39 +0530

    ....!

--- src/G20UITesting.java
+++ src/G20UITesting.java
@@ -12,8 +12,7 @@ public class G20UITesting {
 				"C:\\SeleniumJarAndDriver\\chromedriver_win32\\chromedriver.exe");
 		
 		WebDriver driver= new ChromeDriver();
-
-		
+ 
 		driver.get("https://stackoverflow.com/questions/63996047/eclipse-incompatible-jvm-version-1-8-0-261-of-the-jvm-is-not-suitable-for-th");
 		System.out.println(driver.getTitle());
 		driver.quit();
