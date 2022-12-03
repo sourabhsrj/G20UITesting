01d0d6f96b99e657eac38959e3847347c845f36b
SourabhSrj <sourabhj053@gmail.com>
2022-12-03 09:14:27 +0530

    one time system path and webdriver setup done....!

--- src/OrchidCollegeWebSite/Sample.java
+++ src/OrchidCollegeWebSite/Sample.java
@@ -1,10 +1,13 @@
 package OrchidCollegeWebSite;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Sample {
@@ -12,14 +15,27 @@ public class Sample {
 
 //	WebDriver driver=new ChromeDriver();
 
+
+	WebDriver driver;
+
+	// This method is to navigate flipkart URL
+	@BeforeClass
+	public void init() {
+
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sourabh\\git\\G20UITesting\\src\\drivers\\chromedriver.exe");
+
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.navigate().to("https://www.orchidengg.ac.in");
+	}
+	
+	
+	
 	
 	@Test
 	public void searchButton() {	
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sourabh\\git\\G20UITesting\\src\\drivers\\chromedriver.exe");
-
-		WebDriver driver= new ChromeDriver();
 	
-		driver.get("https://www.orchidengg.ac.in");
 		driver.manage().window().maximize();
 	driver.findElement(By.xpath("//input[@class=\"search-field\"]")).sendKeys("Computer");
 	
@@ -30,21 +46,8 @@ public class Sample {
 	
 	@Test
 	public void entcDepartment() {	
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sourabh\\git\\G20UITesting\\src\\drivers\\chromedriver.exe");
 
-		WebDriver driver= new ChromeDriver();
 
-		driver.manage().window().maximize();
-		driver.get("https://www.orchidengg.ac.in");
-//
-//        WebElement hoverable = driver.findElement(By.className("tc-menu-inner"));
-//       Actions a= new Actions(driver);
-//       
-//                a.moveToElement(hoverable).click().perform();
-//	
-                
-             // Locating the Main Menu (Parent element)
-//                WebElement mainMenu = driver.findElement(By.xpath("//input[@class=\"thim-widget-link\"]"));
 
         WebElement mainMenu = driver.findElement(By.xpath("//*[@id=\"masthead\"]/div/div/div/nav/ul/li[4]/a"));
                 //Instantiating Actions class
@@ -75,21 +78,8 @@ public class Sample {
 	
 	@Test
 	public void contactUs() {	
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sourabh\\git\\G20UITesting\\src\\drivers\\chromedriver.exe");
 
-		WebDriver driver= new ChromeDriver();
 
-		driver.manage().window().maximize();
-		driver.get("https://www.orchidengg.ac.in");
-//
-//        WebElement hoverable = driver.findElement(By.className("tc-menu-inner"));
-//       Actions a= new Actions(driver);
-//       
-//                a.moveToElement(hoverable).click().perform();
-//	
-                
-             // Locating the Main Menu (Parent element)
-//                WebElement mainMenu = driver.findElement(By.xpath("//input[@class=\"thim-widget-link\"]"));
 
         WebElement mainMenu = driver.findElement(By.xpath("//*[@id=\"masthead\"]/div/div/div/nav/ul/li[9]/a"));
                 //Instantiating Actions class
@@ -119,10 +109,7 @@ public class Sample {
                 driver.findElement(By.xpath("//*[@id=\"wpcf7-f8057-p87-o1\"]/form/p[4]/span/textarea")).sendKeys("Mini project testing performed by BE Entc dept student");
                 
 
-//           	 driver.findElement(By.className("search-submit")).click(); 
 
-                
-//                actions.click(mainMenu);
                 
 	}
 	
