83528798ab855d87fdc7ca634dc08d8d7342e534
SourabhSrj <sourabhj053@gmail.com>
2022-12-03 09:09:04 +0530

    implemented 2nd test using mouse hover and mouse actions...In this test is automated it click on departments and then click on Electonics and telecommunication.....!

--- src/OrchidCollegeWebSite/Sample.java
+++ src/OrchidCollegeWebSite/Sample.java
@@ -28,6 +28,47 @@ public class Sample {
 	
 	}
 	
+	@Test
+	public void entcDepartment() {	
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sourabh\\git\\G20UITesting\\src\\drivers\\chromedriver.exe");
+
+		WebDriver driver= new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.get("https://www.orchidengg.ac.in");
+//
+//        WebElement hoverable = driver.findElement(By.className("tc-menu-inner"));
+//       Actions a= new Actions(driver);
+//       
+//                a.moveToElement(hoverable).click().perform();
+//	
+                
+             // Locating the Main Menu (Parent element)
+//                WebElement mainMenu = driver.findElement(By.xpath("//input[@class=\"thim-widget-link\"]"));
+
+        WebElement mainMenu = driver.findElement(By.xpath("//*[@id=\"masthead\"]/div/div/div/nav/ul/li[4]/a"));
+                //Instantiating Actions class
+                Actions actions = new Actions(driver);
+
+                //Hovering on main menu
+
+                actions.moveToElement(mainMenu);
+                actions.moveToElement(mainMenu).click();
+//                actions.
+
+//                // Locating the element from Sub Menu
+                WebElement subMenu = driver.findElement(By.xpath("//*[@id=\"masthead\"]/div/div/div/nav/ul/li[4]/ul/li[4]/a"));
+//
+//                //To mouseover on sub menu
+                actions.moveToElement(subMenu);
+
+                actions.moveToElement(subMenu).click();
+                //build()- used to compile all the actions into a single step 
+                actions.click().build().perform();  
+//                actions.click(mainMenu);
+                
+	}
+	
 	
 	
 	
