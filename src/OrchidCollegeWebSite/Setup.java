b4085249fc46df1ea7f4c1bf4b4333be19ee9b80
SourabhSrj <sourabhj053@gmail.com>
2022-12-03 09:20:26 +0530

    ...!

---
+++ src/OrchidCollegeWebSite/Setup.java
@@ -0,0 +1,27 @@
+package OrchidCollegeWebSite;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Setup {
+	
+	
+	
+	
+	
+	
+	
+
+}
